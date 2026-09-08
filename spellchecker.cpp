07f61e7c23477da273a1cbbe91ce0a8639d93d82
Clayton Smith <clayton.eric.smith@gmail.com>
2026-09-07 20:22:17 -0700

    Implement spell checker using dictionary file
    This program checks a specified file for words not found in a dictionary file (words.txt). It reads words from both files and outputs any words from the checked file that are not present in the dictionary.

--- spellchecker.cpp
+++ spellchecker.cpp
@@ -1,1 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
+
+int main() {
+    // Open words.txt from the local project repository folder
+    ifstream dict_file("words.txt");
+    if (dict_file.fail()) {
+        cout << "Error: Could not open dictionary file words.txt." << endl;
+        cout << "Make sure words.txt is present in the working directory." << endl;
+        return 1;
+    }
+
+    vector<string> words;
+    string word;
+
+    while (dict_file >> word) {
+        words.push_back(word);
+    }
+    dict_file.close();
+
+    string filename;
+    cout << "Enter the name of the file to check: ";
+    cin >> filename;
+
+    ifstream check_file(filename);
+    if (check_file.fail()) {
+        cout << "Error: Could not open file " << filename << endl;
+        return 1;
+    }
+
+    cout << "\nWords not found in dictionary:" << endl;
+
+    while (check_file >> word) {
+        if (find(words.begin(), words.end(), word) == words.end()) {
+            cout << word << endl;
+        }
+    }
+
+    check_file.close();
+    return 0;
+}
